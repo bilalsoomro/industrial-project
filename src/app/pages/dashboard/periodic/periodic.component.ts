ddf9a5a1a92ae348ff75796e34a6493fbcfd116b
Bilal Soomro <bilal.soomro786@gmail.com>
2017-12-04 16:56:51 +0200

    Added mock data and binded model

--- src/app/pages/dashboard/periodic/periodic.component.ts
+++ src/app/pages/dashboard/periodic/periodic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PERIODIC_READINGS_LIST } from '../../../providers/mock-data';
 
 @Component({
   selector: 'app-periodic',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeriodicComponent implements OnInit {
 
+  readingOptions: any = PERIODIC_READINGS_LIST;
+  startDate: Date = new Date();
+  endDate: Date = new Date();
+  chosenReading: any = 'Choose...';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('reading chosen: ', this.chosenReading);
+    console.log("start Date: ", this.startDate);
+    console.log("end Date: ", this.endDate);
   }
 
 }
---
+++ src/app/providers/mock-data.ts
@@ -0,0 +1,24 @@
+//import { Hero } from './hero';
+
+export const PERIODIC_READINGS_LIST: any = [
+  { id: 1, name: 'ACTIVE ENERGY L1' },
+  { id: 2, name: 'ACTIVE ENERGY L2' },
+  { id: 3, name: 'ACTIVE ENERGY L3' },
+  { id: 4, name: 'REACTIVE ENERGY L1' },
+  { id: 5, name: 'REACTIVE ENERGY L2' },
+  { id: 6, name: 'REACTIVE ENERGY L3' }
+];
+
+export const REALTIME_READINGS_LIST: any = [
+    { id: 1, name: 'PHASE VOLTAGE L1-N' },
+    { id: 2, name: 'PHASE VOLTAGE L2-N' },
+    { id: 3, name: 'PHASE VOLTAGE L3-N' },
+    { id: 4, name: 'LINE VOLTAGE L3-1' },
+    { id: 5, name: 'LINE VOLTAGE L3-2' },
+    { id: 6, name: 'LINE VOLTAGE L3-3' },
+    { id: 7, name: 'LINE CURRENT L1' },
+    { id: 8, name: 'LINE CURRENT L2' },
+    { id: 9, name: 'LINE CURRENT L3' }
+    
+    
+  ];
